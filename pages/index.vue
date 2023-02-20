41ddb87c0ffc98d7c02db49990a1a58f3a91274e
Mojtaba Sabbagh <mojtaba.sabbagh@yahoo.com>
2023-02-20 22:44:41 +0330

    First commit.

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-col items-center mt-10">
+    <div>
+      <textarea class="border-2 p-2" id="source" name="source" 
+        rows="4" cols="50" :placeholder="source"></textarea>
+    </div>
+    <div>
+      <button> <img src="~/assets/images/nn.png"/></button>
+    </div>
+    <div>
+      <textarea class="border-2 text-right p-2" id="source" name="source" 
+        rows="4" cols="50" :placeholder="destination"></textarea>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+          return {
+            source: 'English',
+            destination: 'فارسی'
+          }
+        }
+}
+</script>
